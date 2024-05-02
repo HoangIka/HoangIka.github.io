286ed2d229d853e7b33c350ca7339b26396f052d
HoangIka <86615257+HoangIka@users.noreply.github.com>
2024-05-02 12:02:18 +0700

    Add files via upload

--- script.js
+++ script.js
@@ -2,6 +2,37 @@ const map = [{img: "image1.jpeg", audio:"scares1.mp3",},
              {img:"image2.jpeg",audio:"scares2.mp3",}
 ];
 
+const contain = document.querySelector(".container");
+var img = document.createElement("img");
+var audio = document.createElement("audio");
+
+/*---------------------------onclick---------------------------*/
+function active(){
+    console.log("actived!");
+    
+    /*const contain = document.querySelector(".container");
+    let img = document.createElement("img");
+    let audio = document.createElement("audio");
+    
+    const choose = randomized(2);
+    
+    setupImg(img,choose);
+    setupAudio(audio,choose);*/
+    
+    document.body.insertBefore(img,contain);
+    document.body.insertBefore(audio,contain);
+    
+    audio.load();
+    audio.play().then(() => {
+        console.log("Audio playing automatically");
+    }).catch(error => {
+        console.log("Error occurred during audio playback:", error.message);
+    });
+    
+    setInterval(()=>{window.location.href = "./index.html";}
+    ,4000);
+}
+/*---------------------------function---------------------------*/
 function randomized(max){
     return Math.floor(Math.random() * max);
 }
@@ -26,50 +57,52 @@ function setupAudio(audio,choose){
     audio.setAttribute("controls","");
     
     audio.style.display="none";
+}
+/*---------------------------------------------------------------*/
+let count = 10, counter = true, doing = false;
+
+function time(){
+        setInterval(()=>{
+            if(!counter||!doing){
+                return;
+            }
+            let timer = document.querySelector(".timer");
+            /*console.log(count);*/
+            if(1){
+                timer.style.display = "inline";
+                timer.innerText = count + "s";
+                --count;
+                if(count<0){
+                    document.body.insertBefore(img,contain);
+    document.body.insertBefore(audio,contain);
     
     audio.load();
     audio.play().then(() => {
         console.log("Audio playing automatically");
     }).catch(error => {
-        console.log("Error occurred during audio playback:", error);
+        console.log("Error occurred during audio playback:", error.message);
     });
-}
-
-function active(){
-    console.log("actived!");
     
-    const contain = document.querySelector(".container");
-    let img = document.createElement("img");
-    let audio = document.createElement("audio");
-    
-    const choose = randomized(2);
-    
-    setupImg(img,choose);
-    setupAudio(audio,choose);
-    
-    document.body.insertBefore(img,contain);
-    document.body.insertBefore(audio,contain);
-}
-
-let count = 20, counter = true;
-
-function time(){
-        setInterval(()=>{
-            if(!counter){
-                return;
-            }
-            let timer = document.querySelector(".timer");
-            --count;
-            /*console.log(count);*/
-            if(count<=10){
-                timer.style.display = "inline";
-                timer.innerText = count + "s";
-                
-                if(count<0){
-                    active();
+    setInterval(()=>{window.location.href = "./index.html";}
+    ,4000);
                     counter = false;
                 }
             }
         },1000);
+}
+function visible(){
+    doing = true;
+    let part1 = document.querySelector("#part-visible");
+    let part2 = document.querySelector("#part-hidden");
+    let aud = document.querySelector("#aud");
+    
+    aud.play();
+    part2.id = "";
+    part1.style.display = "none";
+    
+    const choose = randomized(2);
+
+    setupAudio(audio,choose);
+    setupImg(img,choose);
 }
 time();
